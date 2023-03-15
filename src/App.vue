6aefd055a8132f58723778a3997e81eb59243edc
Eduardo Diaz <edudug98@gmail.com>
2023-03-15 20:59:15 +0000

    crear app en vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import DigitalClock from './components/DigitalClock.vue';
+
+export default {
+  name: "App",
+  components: {
+    DigitalClock
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <DigitalClock></DigitalClock>
+  </div>
+</template>
+
+<style>
+  
+</style>
---
+++ src/components/DigitalClock.vue
@@ -0,0 +1,32 @@
+<script>
+    export default{
+        name: 'DigitalClock',
+        mounted() { 
+            setInterval(() => this.showTime(), 1000);
+         },
+        data() {
+            return {
+                hours: '',
+                minutes: '',
+                seconds: ''
+            }
+        },
+        methods: {
+            showTime() {
+                const date = new Date();
+                this.hours = date.getHours()
+                this.minutes = date.getMinutes()
+                this.seconds = date.getSeconds()
+                console.log('Se monta el componente')
+            }
+        }
+    }
+</script>
+
+<template>
+    <span> {{ `${hours} : ${minutes} : ${seconds}`}} </span>
+</template>
+
+<style>
+
+</style>
